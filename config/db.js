a9d642af7e791f14d2d9783306abca331b0df213
mernxl <mernxl@gmail.com>
2020-10-15 11:13:43 +0100

    chore: replace dotevn with `@xelgrp/configu` for loading configurations, add dev config for mongodb

--- config/db.js
+++ config/db.js
@@ -1,20 +1,25 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const configu =  require( '@xelgrp/configu');
 
-const ConnectDB = async () => {
-    try {
-    
-    mongoose.connect(process.env.MONGO_LOCAL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    });
-    console.log('MongoDB Connected...');
-    } catch(err) {
-        console.error(err);
-    }
+const mongoose = require('mongoose');
+
+const config = configu.loadConfig('app.ini', {});
+
+const ConnectDB = () => {
+  try {
+    mongoose.connect(config.mongodb.uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      user: config.mongodb.user,
+      pass: config.mongodb.pass,
+      dbName: config.mongodb.dbName,
+    })
+      .then(() => console.log('Mongodb connected...'))
+      .catch(err => console.log(err));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-module.exports = ConnectDB; 
+module.exports = ConnectDB
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
-// const ConnectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
 const port = process.env.PORT || 5000;
 const app = express();
 
-if(process.env.NODE_ENV != 'production'){
-  require('dotenv').config();
-}
+const ConnectDB = require('./config/db');
+// Connect to mongo database
+ConnectDB();
 
 const routes = require('./routes/index');
 const summerRoute = require('./routes/summerForm');
@@ -20,22 +17,11 @@ const artRouter = require('./routes/articles');
 const auth = require('./routes/auth');
 const adminRoute = require('./routes/admin');
 
-// Connection to database
-// process.env.MONGODB_URI
-// process.env.MONGO_LOCAL
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-  }).then(() => console.log('Mongodb connected...'))
-  .catch(err => console.log(err));
-
 // To parse form data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ extended: false }));
 
-// Connect to mongo database
-// ConnectDB();
-
-// to display our web pages  
+// to display our web pages
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
@@ -46,7 +32,7 @@ app.use(methodOverride('_method'));
 // static folders
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/ups')));
- 
+
 app.use('/', artRouter);
 app.use('/', routes);
 app.use('/summer', summerRoute);
@@ -54,5 +40,5 @@ app.use('/summer', summerRoute);
 app.use('/', auth);
 app.use('/admin', adminRoute);
 
-// create server 
-app.listen(port, () => console.log('Server is listening on port ' + port));  
+// create server
+app.listen(port, () => console.log('Server is listening on port ' + port));
